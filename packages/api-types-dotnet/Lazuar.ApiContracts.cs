90f1af166d07d00b4f239953bb1e383dd7e2024a
Akmal Firdaus <xkmxlfirdxus@gmail.com>
2026-06-11 00:01:18 +0800

    WIP: Thu Jun 11 00:01:18 +08 2026

--- packages/api-types-dotnet/Lazuar.ApiContracts.cs
+++ packages/api-types-dotnet/Lazuar.ApiContracts.cs
@@ -1872,6 +1872,24 @@ namespace Lazuar.ApiTypes
     }
 
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
+    public partial class RenameConversationRequestDto
+    {
+
+        [System.Text.Json.Serialization.JsonPropertyName("title")]
+        public string Title { get; set; } = default!;
+
+        private System.Collections.Generic.IDictionary<string, object>? _additionalProperties;
+
+        [System.Text.Json.Serialization.JsonExtensionData]
+        public System.Collections.Generic.IDictionary<string, object> AdditionalProperties
+        {
+            get { return _additionalProperties ?? (_additionalProperties = new System.Collections.Generic.Dictionary<string, object>()); }
+            set { _additionalProperties = value; }
+        }
+
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
     public partial class Response
     {
 
