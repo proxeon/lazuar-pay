369933cfdc839e202334aac3ce64f9224d8e327d
Akmal Firdaus <xkmxlfirdxus@gmail.com>
2026-06-17 01:57:20 +0800

    WIP: Wed Jun 17 01:57:20 +08 2026

--- scripts/lhdn_sandbox/run_all.sh
+++ scripts/lhdn_sandbox/run_all.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+# Make the new script executable
+chmod +x 06_test_cancel.sh
+
 ./00_provision.sh && \
 ./01_test_b2b.sh && \
 ./02_test_credit_note.sh && \
-./03_test_b2c.sh
+./03_test_b2c.sh && \
+./06_test_cancel.sh
 # ./04_upload_dummy_cert.sh && \
 # ./05_test_b2b_v1_1.sh
 
