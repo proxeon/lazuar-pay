7d477492b57d16477c078f3b860e1c44a0d9d981
Akmal Firdaus <xkmxlfirdxus@gmail.com>
2026-06-26 23:13:52 +0800

    WIP: Fri Jun 26 23:13:52 +08 2026

---
+++ apps/lazuar-api/Modules/Community/Infrastructure/Migrations/20260626151146_AddProductTypeAndFulfillmentUrl.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Community.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTypeAndFulfillmentUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FulfillmentFileUrl",
+                schema: "community",
+                table: "Plans",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductType",
+                schema: "community",
+                table: "Plans",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FulfillmentFileUrl",
+                schema: "community",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "ProductType",
+                schema: "community",
+                table: "Plans");
+        }
+    }
+}
