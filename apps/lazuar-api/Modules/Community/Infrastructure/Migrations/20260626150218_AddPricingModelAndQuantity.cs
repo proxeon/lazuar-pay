c5c232c8a6abb3e644e702870ed0e9f82f1ba5c3
Akmal Firdaus <xkmxlfirdxus@gmail.com>
2026-06-26 23:03:34 +0800

    WIP: Fri Jun 26 23:03:34 +08 2026

---
+++ apps/lazuar-api/Modules/Community/Infrastructure/Migrations/20260626150218_AddPricingModelAndQuantity.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Community.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPricingModelAndQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                schema: "community",
+                table: "Subscriptions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PricingModel",
+                schema: "community",
+                table: "Plans",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                schema: "community",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "PricingModel",
+                schema: "community",
+                table: "Plans");
+        }
+    }
+}
