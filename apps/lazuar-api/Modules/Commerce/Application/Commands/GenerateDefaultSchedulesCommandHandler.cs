2b0daa695db60ffff1dd0b29c1b7a4997ae40ec0
Akmal Firdaus <xkmxlfirdxus@gmail.com>
2026-06-29 03:33:53 +0800

    Phase 2: Refactor the Command Handler (Application Layer)

--- apps/lazuar-api/Modules/Commerce/Application/Commands/GenerateDefaultSchedulesCommandHandler.cs
+++ apps/lazuar-api/Modules/Commerce/Application/Commands/GenerateDefaultSchedulesCommandHandler.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
 using BuildingBlocks.Application;
-using Dapper;
-using Microsoft.Extensions.DependencyInjection;
 using Modules.Commerce.Contracts.Commands;
 using Modules.Commerce.Domain.Aggregates;
 
@@ -14,34 +10,15 @@ namespace Modules.Commerce.Application.Commands;
 public class GenerateDefaultSchedulesCommandHandler : ICommandHandler<GenerateDefaultSchedulesCommand>
 {
     private readonly ICommerceRepository _repository;
-    private readonly ISqlConnectionFactory _sqlConnectionFactory;
 
-    public GenerateDefaultSchedulesCommandHandler(
-        ICommerceRepository repository,
-        [FromKeyedServices("CommerceSqlConnectionFactory")] ISqlConnectionFactory sqlConnectionFactory)
+    public GenerateDefaultSchedulesCommandHandler(ICommerceRepository repository)
     {
         _repository = repository;
-        _sqlConnectionFactory = sqlConnectionFactory;
     }
 
     public async Task Handle(GenerateDefaultSchedulesCommand request, CancellationToken ct)
     {
-        using var connection = _sqlConnectionFactory.CreateConnection();
-        if (connection.State != ConnectionState.Open) connection.Open();
-
-        const string query = @"
-            SELECT ""Id"", ""Name"" 
-            FROM communications.""MessageTemplates"" 
-            WHERE ""OrganizationId"" = @TenantId 
-              AND ""Name"" IN ('Subscription Renewal (3 Days)', 'Subscription Renewal Due Today', 'Subscription Renewal Overdue')";
-
-        var templates = await connection.QueryAsync<(Guid Id, string Name)>(query, new { TenantId = request.OrganizationId });
-        var templateDict = new Dictionary<string, Guid>();
-
-        foreach (var t in templates)
-        {
-            templateDict[t.Name] = t.Id;
-        }
+        var templateDict = await _repository.GetDefaultTemplateIdsAsync(request.OrganizationId, ct);
 
         if (templateDict.TryGetValue("Subscription Renewal (3 Days)", out var preTemplateId))
         {
