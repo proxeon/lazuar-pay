6be368880eab9644223bfbbb08ffa239ebbf649d
Akmal Firdaus <xkmxlfirdxus@gmail.com>
2026-06-30 18:41:24 +0800

    WIP: Tue Jun 30 18:41:24 +08 2026

---
+++ apps/lazuar-api/Modules/Commerce/Infrastructure/Migrations/20260630104003_AddAdHocLineItemsToCheckoutSession.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Commerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdHocLineItemsToCheckoutSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                schema: "commerce",
+                table: "CheckoutSessions",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdHocLineItems",
+                schema: "commerce",
+                table: "CheckoutSessions",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsB2bRequired",
+                schema: "commerce",
+                table: "CheckoutSessions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdHocLineItems",
+                schema: "commerce",
+                table: "CheckoutSessions");
+
+            migrationBuilder.DropColumn(
+                name: "IsB2bRequired",
+                schema: "commerce",
+                table: "CheckoutSessions");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                schema: "commerce",
+                table: "CheckoutSessions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
